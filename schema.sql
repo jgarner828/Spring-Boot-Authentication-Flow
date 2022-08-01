2edfb996fbca64f90b81408981b1f91ac4e5434b
Justin Garner <j.garner828@gmail.com>
2022-08-01 15:27:24 -0400

    added basic mysql schema for db

---
+++ schema.sql
@@ -0,0 +1,23 @@
+create schema if not exists basicuserDB;
+use basicuserDB;
+
+create table if not exists users(
+    username varchar(50) not null primary key,
+    password varchar(100) not null,
+    enabled boolean not null
+);
+
+create table if not exists authorities (
+    username varchar(50) not null,
+    authority varchar(50) not null,
+    constraint fk_authorities_users foreign key(username) references users(username));
+    create unique index ix_auth_username on authorities (username,authority
+);
+
+insert into users (username, password, enabled) values ('justin', '$2a$10$KxTc8SYbIB/IaXCWz6NA4ug1pkAYM/e.P.0YQFGE3Ua4FZ6Qf842a', true);
+insert into users (username, password, enabled) values ('user', '$2a$10$QPnaeWBWz1BdDglni2CLzO2YMeifVXtQDPgUOVNETTcj8cEGwqiym', true);
+insert into users (username, password, enabled) values ('admin', '$2a$10$Hc878CPLJ4hOtwyzt6V7..LHtzhcR3zqcXOAPseY9QGg05ZxcsTR6', true);
+
+insert into authorities (username, authority) values ('user', 'ROLE_USER');
+insert into authorities (username, authority) values ('admin', 'ROLE_ADMIN');
+insert into authorities (username, authority) values ('justin', 'ROLE_ADMIN');
